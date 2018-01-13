536149660b12def8c360e9165ed1acc30a03858d
awmleer <awmleer@sparker.xyz>
2018-01-13 17:11:17 +0800

    hist

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 using namespace cv;
 using namespace std;
 
-double getPSNR(const Mat& I1, const Mat& I2);
-double getMSSIM( const Mat& i1, const Mat& i2);
+double getHIST( const Mat& i1, const Mat& i2);
+double getPSNR(const Mat& i1, const Mat& i2);
 
 
 int main(int argc, char** argv ) {
@@ -17,7 +17,7 @@ int main(int argc, char** argv ) {
     //get thresh
     double thresh;
     thresh = atof(argv[3]);
-    //get method (0 represents for PSNR, 1 represents for MSSIM
+    //get method (0 represents for HIST, 1 represents for PSNR
     int method = atoi(argv[2]);
     //video reader
     VideoCapture capture = VideoCapture(videoPath);
@@ -42,9 +42,9 @@ int main(int argc, char** argv ) {
         if (count > 1) {
             double diff;
             if(method==0){
-                diff = getPSNR(frameA, frameB);
+                diff = getHIST(frameA, frameB);
             }else{
-                diff = getMSSIM(frameA, frameB);
+                diff = getPSNR(frameA, frameB);
             }
 //            cout<<diff<<endl;
             if (diff < thresh) {
@@ -62,11 +62,30 @@ int main(int argc, char** argv ) {
     return 0;
 }
 
-double getPSNR(const Mat& I1, const Mat& I2) {
+
+double getHIST( const Mat& i1, const Mat& i2){
+    int channels[] = { 0, 1 };
+    int histSize[] = { 50, 60 };
+    float h_ranges[] = { 0, 180 };
+    float s_ranges[] = { 0, 256 };
+    const float* ranges[] = { h_ranges, s_ranges };
+    Mat hsv1, hsv2;
+    MatND hist1, hist2;
+    cvtColor( i1, hsv1, COLOR_BGR2HSV );
+    cvtColor( i2, hsv2, COLOR_BGR2HSV );
+    calcHist( &hsv1, 1, channels, Mat(), hist1, 2, histSize, ranges, true, false );
+    calcHist( &hsv2, 1, channels, Mat(), hist2, 2, histSize, ranges, true, false );
+    normalize( hist1, hist1, 0, 1, NORM_MINMAX, -1, Mat() );
+    normalize( hist2, hist2, 0, 1, NORM_MINMAX, -1, Mat() );
+    return compareHist(hist1, hist2, 0);
+}
+
+
+double getPSNR(const Mat& i1, const Mat& i2) {
     Mat s1;
-    absdiff(I1, I2, s1);       // |I1 - I2|
+    absdiff(i1, i2, s1);       // |i1 - i2|
     s1.convertTo(s1, CV_32F);  // cannot make a square on 8 bits
-    s1 = s1.mul(s1);           // |I1 - I2|^2
+    s1 = s1.mul(s1);           // |i1 - i2|^2
 
     Scalar s = sum(s1);         // sum elements per channel
 
@@ -76,61 +95,8 @@ double getPSNR(const Mat& I1, const Mat& I2) {
         return 0;
     else
     {
-        double  mse =sse /(double)(I1.channels() * I1.total());
+        double  mse =sse /(double)(i1.channels() * i1.total());
         double psnr = 10.0*log10((255*255)/mse);
         return psnr;
     }
-}
-
-double getMSSIM( const Mat& i1, const Mat& i2)
-{
-    const double C1 = 6.5025, C2 = 58.5225;
-    /***************************** INITS **********************************/
-    int d     = CV_32F;
-
-    Mat I1, I2;
-    i1.convertTo(I1, d);           // cannot calculate on one byte large values
-    i2.convertTo(I2, d);
-
-    Mat I2_2   = I2.mul(I2);        // I2^2
-    Mat I1_2   = I1.mul(I1);        // I1^2
-    Mat I1_I2  = I1.mul(I2);        // I1 * I2
-
-    /***********************PRELIMINARY COMPUTING ******************************/
-
-    Mat mu1, mu2;   //
-    GaussianBlur(I1, mu1, Size(11, 11), 1.5);
-    GaussianBlur(I2, mu2, Size(11, 11), 1.5);
-
-    Mat mu1_2   =   mu1.mul(mu1);
-    Mat mu2_2   =   mu2.mul(mu2);
-    Mat mu1_mu2 =   mu1.mul(mu2);
-
-    Mat sigma1_2, sigma2_2, sigma12;
-
-    GaussianBlur(I1_2, sigma1_2, Size(11, 11), 1.5);
-    sigma1_2 -= mu1_2;
-
-    GaussianBlur(I2_2, sigma2_2, Size(11, 11), 1.5);
-    sigma2_2 -= mu2_2;
-
-    GaussianBlur(I1_I2, sigma12, Size(11, 11), 1.5);
-    sigma12 -= mu1_mu2;
-
-    ///////////////////////////////// FORMULA ////////////////////////////////
-    Mat t1, t2, t3;
-
-    t1 = 2 * mu1_mu2 + C1;
-    t2 = 2 * sigma12 + C2;
-    t3 = t1.mul(t2);              // t3 = ((2*mu1_mu2 + C1).*(2*sigma12 + C2))
-
-    t1 = mu1_2 + mu2_2 + C1;
-    t2 = sigma1_2 + sigma2_2 + C2;
-    t1 = t1.mul(t2);               // t1 =((mu1_2 + mu2_2 + C1).*(sigma1_2 + sigma2_2 + C2))
-
-    Mat ssim_map;
-    divide(t3, t1, ssim_map);      // ssim_map =  t3./t1;
-
-    Scalar mssim = mean( ssim_map ); // mssim = average of ssim map
-    return (mssim[0]+mssim[1]+mssim[2])/3;
 }
